c568808acabf9dbb356162a05fee98783e7e02f2
Laksh <lakshtaneja@yahoo.com>
2025-05-23 16:18:12 +0530

    home_page.dart add timestring to blocks

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,9 +8,43 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "${DateTime.now().day.toString()} ${DateTime.now().month.toString()} ${DateTime.now().year.toString()}",
+          "${DateTime.now().day.toString()}/${DateTime.now().month.toString()}/${DateTime.now().year.toString()}",
         ),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: 24,
+        itemBuilder: (BuildContext context, int index) {
+          String timeString;
+
+          if (index < 9) {
+            timeString = "0$index:00 - 0${index + 1}:00";
+          } else if (index == 9) {
+            timeString = "0$index:00 - ${index + 1}:00";
+          } else {
+            timeString = "$index:00 - ${index + 1}:00";
+          }
+
+          return Timeblock(timeString: timeString);
+        },
+      ),
+    );
+  }
+}
+
+class Timeblock extends StatelessWidget {
+  const Timeblock({super.key, required this.timeString});
+
+  final String timeString;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(border: BorderDirectional(top: BorderSide())),
+      height: MediaQuery.of(context).size.height / 6,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(alignment: Alignment.bottomRight, child: Text(timeString)),
       ),
     );
   }
